f0bd6a713ddaa63636244ceb71dee82f14671410
noelart <noelart@maltanet.net>
2014-09-04 18:43:05 +0000

    Add Jquery animation to landing page

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -96,52 +96,43 @@ require("./landing");
 
 ;require.register("scripts/landing", function(exports, require, module) {
 /* JQuery */
-$(document).ready(function () {
+$(document).ready (function() {
 	$('.hero-content h3').click(function() {
 		subText = $(this).text();
-		$(this).text(subText + "*");
-		console.log(subText);
+		$(this).text(subText + " * ");
 	});
 
 	$('.selling-points .point h5').click(function() {
-		$(this).css({'font-size' : '1.8em', 'text-transform' : 'uppercase' });
-		console.log('Capitalize selling-points h5');
+		$(this).css({'font-size' : '24pt', 'text-transform' : 'uppercase'});
 	});
 
-	$('.hero-content').click(function() {
-		$(this).fadeOut(2000, function() {
-			$(this).fadeIn(2000, function() {
-				$(this).css({background : 'url(/images/band_hero2.jpg)', 'background-position' : '50% 10%', 'background-size' : 'cover'});
-			});
+	$('.hero-content').click(function () {
+		$(this).fadeIn('slow', function() {
+			$(this).css({'background' : 'url(/images/band_hero2.jpg)', 'background-position' : '50% 10%', 'background-size' : 'cover'});
 		});
-		console.log('Changed background image');
 	});
-
-/*	var band2Image = */
 
 	var onHoverShiftRight = function() {
 		$(this).animate({'margin-left' : '100px'});
-		console.log("hover action triggered");
 	};
 
 	var offHoverShiftLeft = function() {
-		$(this).animate({'margin-left' : '0'});
-		console.log("Hover off action");
+		$(this).animate({'margin-left' : '0'})
 	};
 
-	var onHoverActionHeader = function() {
-		$(this).css({color : 'yellow'});
-		console.log('Changed h1 colour to yellow');
+	var onHoverTurnYellow = function() {
+		$(this).css({'color' : 'yellow'});
 	};
 
-	var offHoverActionHeader = function() {
-		$(this).css({color : '#EB7F00'});
-		console.log('h1 reverted to original colour');
-	};
+	var offHoverTurnOrange = function() {
+		$(this).css({'color' : '#EB7F00'});
+	}
 
-	$('.navbar img').hover(onHoverShiftRight, offHoverShiftLeft);
-	$('.hero-content h1').hover(onHoverActionHeader, offHoverActionHeader);
+	$('.navbar .navbar-header img').hover(onHoverShiftRight, offHoverShiftLeft);
+	$('.hero-content h1').hover(onHoverTurnYellow, offHoverTurnOrange);
 });
+
+
 });
 
 ;require.register("server", function(exports, require, module) {
